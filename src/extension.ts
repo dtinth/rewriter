653b4f7f305eddc3b79c190ed520575b3dace09b
dtinth on MBP M1 <dtinth@spacet.me>
2024-01-25 03:58:03 +0700

    Initial implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,135 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rewriter.setApiKey", async () => {
+      const apiKey = await vscode.window.showInputBox({
+        prompt: "Please enter the API key generated from Google AI Studio",
+        placeHolder: "API Key",
+        password: true,
+        ignoreFocusOut: true,
+      });
+      if (!apiKey) {
+        return;
+      }
+      await context.secrets.store("rewriter.apiKey", apiKey);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rewriter.rewrite", async (args) => {
+      const task = vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: "Rewriting...",
+          cancellable: false,
+        },
+        async (progress) => {
+          const apiKey = await context.secrets.get("rewriter.apiKey");
+          if (!apiKey) {
+            vscode.window.showErrorMessage(
+              "Please set the API key first by running the command Rewriter: Set API Key"
+            );
+            return;
+          }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fresh-extension" is now active!');
+          const editor = vscode.window.activeTextEditor;
+          if (!editor) {
+            vscode.window.showErrorMessage("No active editor found");
+            return;
+          }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fresh-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fresh-extension!');
-	});
+          const selection = editor.selection;
+          const text = editor.document.getText(selection);
+          if (!text) {
+            vscode.window.showErrorMessage("No text selected");
+            return;
+          }
 
-	context.subscriptions.push(disposable);
+          const parts: { text: string }[] = [];
+          parts.push({
+            text:
+              args?.prompt ||
+              "Please rewrite the text to improve its grammar and readability",
+          });
+          const examples: { input: string; output: string }[] =
+            args?.examples || [
+              {
+                input:
+                  "Wildcard can capture the value after segment regardless of amount by using `*`.",
+                output:
+                  "Using `*` as a wildcard allows capturing any value after a segment, irrespective of its length or content.",
+              },
+              {
+                input: "This library is build on top of web standards.",
+                output: "This library is built on top of web standards.",
+              },
+            ];
+          if (Array.isArray(examples)) {
+            for (const example of examples) {
+              parts.push({ text: `input: ${example?.input}` });
+              parts.push({ text: `output: ${example?.output}` });
+            }
+          }
+          parts.push({ text: `input: ${text}` });
+          parts.push({ text: `output: ` });
+
+          const request = {
+            contents: [{ parts }],
+            generationConfig: {
+              temperature: 0.9,
+              topK: 1,
+              topP: 1,
+              maxOutputTokens: 2048,
+              stopSequences: [],
+            },
+            safetySettings: [
+              {
+                category: "HARM_CATEGORY_HARASSMENT",
+                threshold: "BLOCK_NONE",
+              },
+              {
+                category: "HARM_CATEGORY_HATE_SPEECH",
+                threshold: "BLOCK_NONE",
+              },
+              {
+                category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                threshold: "BLOCK_NONE",
+              },
+              {
+                category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+                threshold: "BLOCK_NONE",
+              },
+            ],
+          };
+          if (args?.debug) {
+            console.log("Request to Gemini", request);
+          }
+
+          const response = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?${new URLSearchParams(
+              { key: apiKey }
+            )}`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(request),
+            }
+          );
+          const data = await response.json();
+          if (args?.debug) {
+            console.log("Response from Gemini", data);
+          }
+
+          const output = (data as any).candidates[0].content.parts
+            .map((part: { text: string }) => part.text)
+            .join("");
+          await editor.edit((editBuilder) => {
+            editBuilder.replace(selection, output);
+          });
+        }
+      );
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
